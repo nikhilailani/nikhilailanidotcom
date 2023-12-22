234fccb80d01ab0f80e8db12eea7b721b9977ef7
said7388 <mdabusaidriyaz@gmail.com>
2023-12-22 15:47:55 +0600

    update content

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -34,7 +34,7 @@ export const experiences = [
   },
   {
     id: 3,
-    title: "Self-Employed Developer",
+    title: "Self Employed",
     company: "Code and build something in everyday.",
     duration: "(Jan 2018 - Present)"
   }
