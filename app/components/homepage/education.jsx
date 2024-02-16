1d105f81a98e6256fd809bc19a83065fa298f7e5
said7388 <mdabusaidriyaz@gmail.com>
2024-02-16 16:37:17 +0600

    update projects

--- app/components/homepage/education.jsx
+++ app/components/homepage/education.jsx
@@ -8,10 +8,14 @@ import lottieFile from '/public/lottie/study.json';
 
 function Education() {
   return (
-    <div className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-
-      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
-
+    <div id="education" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+      <Image
+        src="/section.svg"
+        alt="Hero"
+        width={1572}
+        height={795}
+        className="absolute top-0 -z-10"
+      />
       <div className="flex justify-center -translate-y-[1px]">
         <div className="w-3/4">
           <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -4,13 +4,14 @@ import SingleProject from './single-project';
 const Projects = () => {
 
   return (
-    <div className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-
-      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
-
-      <div className="flex justify-center -translate-y-[1px]">
-        <div className="w-3/4">
-          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
+    <div id='projects' className="relative z-50  my-12 lg:my-24">
+      <div className="sticky top-10">
+        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+        <div className="flex items-center justify-start relative">
+          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+            PROJECTS
+          </span>
+          <span className="w-full h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
 
