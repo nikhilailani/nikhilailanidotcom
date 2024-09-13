4b53e3494d6d078dda5d424f13990d9e80ca457d
nikhilailani <nikzz1310@gmail.com>
2024-09-13 15:41:27 -0400

    Fix: Data Analyst text

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,10 +8,11 @@ export default function Profile() {
         <div className="grid gap-2 grid-cols-1 lg:grid-cols-[1fr_0.5fr] items-start mb-2">
 
             <div className="flex flex-col bg-bento2 p-6 h-full rounded-[15px]">
-                <h1 className="text-5xl font-bold justify-between mt-auto mb-2"> Data Analyst </h1>
-                <span className="text-2xl">
-                    with nearly 2 years of experience in Java Spring, Flask and data processing
+                <h1 className="text-5xl text-xl-left text-center justify-between mt-auto mb-2"> Data Analyst 
+                <span className="text-2xl text-center">
+                    &nbsp; with nearly 2 years of experience in Java Spring, Flask and data processing
                 </span>
+                </h1>
             </div>
 
             <div className="flex flex-col bg-bento3 rounded-[15px]">
